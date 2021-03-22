f0ddf7ddd19ad8f1155bb03cf443ae9accb82798
Abhishek Patel <abhipatel8719@gmail.com>
2021-03-22 12:34:54 +0530

    my first commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
-import axios from 'axios' 
+import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,157 +13,157 @@ const useStyles = makeStyles((theme) => ({
 
 function Contact() {
 
-    const classes = useStyles();
-   const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-   const handleClick = () => {
-     setOpen(true);
-   };
- 
-   const handleClose = (event, reason) => {
-     if (reason === 'clickaway') {
-       return;
-     }
- 
-     setOpen(false);
-   };
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-    const resetForm=()=>{
-        setName("")
-        setEmail("")
-        setPhone("")
-        setMessage("")
-    
-        setTimeout(() => {
-          // setSent(false)
-        }, 3000);
-      }
-    
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-    const sendMail=(e)=>{
-        e.preventDefault();
-        // console.log(phone)
-        
-        let data={
-          name:name,
-          email:email,
-          phone:phone,
-          message:message
-        }
-        console.log(data);
-          
-        axios.post('http://localhost:1337/form',data).then(res=>{
-          console.log("innnnn")
-          handleClick();
-          resetForm();
-        }).catch(()=>{
-          alert("check your internet connection!")
-          // console.log("message not sent!!!")
-        })
-    
-        axios.post('http://localhost:1337/formdatabase',data).then(res=>{
-    
-        }).catch(()=>{
-          // console.log("message not saved to database!!!")
-          alert("message not saved to database!!!");
-        })
-        
-      }
-    
-      const [name,setName]=useState("");
-      const [email,setEmail]=useState("");
-      const [phone,setPhone]=useState("");
-      const [message,setMessage]=useState("");
-    
+    setOpen(false);
+  };
 
-    return (
-      
-<div className="container">
+  const resetForm = () => {
+    setName("")
+    setEmail("")
+    setPhone("")
+    setMessage("")
 
-    <br />
-    
-    <div className="row">
-   
+    setTimeout(() => {
+      // setSent(false)
+    }, 3000);
+  }
+
+
+  const sendMail = (e) => {
+    e.preventDefault();
+    // console.log(phone)
+
+    let data = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message
+    }
+    console.log(data);
+
+    axios.post('http://localhost:1337/form', data).then(res => {
+      console.log("innnnn")
+      handleClick();
+      resetForm();
+    }).catch(() => {
+      alert("check your internet connection!")
+      // console.log("message not sent!!!")
+    })
+
+    axios.post('http://localhost:1337/formdatabase', data).then(res => {
+
+    }).catch(() => {
+      // console.log("message not saved to database!!!")
+      alert("message not saved to database!!!");
+    })
+
+  }
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+
+  return (
+
+    <div className="container">
+
+      <br />
+
+      <div className="row">
+
         <div className="col-md-6">
-        <div class="jumbotron"><h1>Contact Us </h1> </div>
-            <form className="my-form">
-                <div className="form-group">
-                <i class="fa fa-user icon">  </i>   &nbsp;&nbsp;
+          <div class="jumbotron"><h1>Contact Us </h1> </div>
+          <form className="my-form">
+            <div className="form-group">
+              <i class="fa fa-user icon">  </i>   &nbsp;&nbsp;
                     <label for="form-name">Name</label>
-                    <input
-                    name="name"
-                    value={name}
-                    onChange={e=>setName(e.target.value)}
-                    type="name" className="form-control" id="form-name" placeholder="Name" required/>
-                </div>
-                <div className="form-group">
-                <i class="fa fa-envelope icon ">  </i> &nbsp;&nbsp;
-                    <label for="form-email">Email</label>
-                    <input
-                    name="email"
-                    value={email}
-                    onChange={e=>setEmail(e.target.value)}
-                    type="email" className="form-control" id="form-email" placeholder="Email Address" required/>
-                </div>
-                <div className="form-group">
-                <i class="fas fa-phone"></i>&nbsp;&nbsp;
-                    <label for="form-subject">Contact Number</label>
-                    <input
-                    name="phone"
-                    value={phone}
-            onChange={e=>setPhone(e.target.value)}
-                    type="text" className="form-control" id="form-subject" placeholder="Enter Your Contact No." required/>
-                </div>
-                <div className="form-group">
-                    <label for="form-message">Email your Message</label>
-                    <textarea
-                    name="message"
-                    value={message}
-            onChange={e=>setMessage(e.target.value)}
-                    className="form-control" id="form-message" placeholder="Enter Your Message"></textarea>
-                </div>
-                <button className="btn btn-dark" type="submit">Send <strong><i class="fas fa-greater-than"></i></strong></button>   
-               
-                <br/>     
-                <br/>       
-                <br/>      
-               
-            </form>
-           
-        </div>
-     
-        
-        <div className="col-md-6">
-        <div class="jumbotron"><h1>Our Office</h1></div>
-        
-                <div class="panel panel-default">
-                    <div class="text-center header">Our Office</div>
-                    <div class="panel-body text-center">
-                    <h2><i class="far fa-map">  <strong>    Address </strong>   </i></h2>
-                    <h6>156/3/2 Bilankheda </h6>
-                    <h6>Khandwa</h6>
-                     <hr  style={{backgroundColor:"#808080"}} />
-                       
-                        <h2><i class="fas fa-phone-alt">  <strong>    Phone </strong>   </i></h2>
-                    <h6><a href='tel:8955162252' style={{color:"black"}}>+91 9424874278</a></h6>
-                    <h6>+91 9999999999 </h6>
-                    <hr  style={{backgroundColor:"#808080"}} />
-        
-
-                        <h2><i class="fa fa-envelope icon">  <strong>   Email</strong>   </i></h2>
-                        <h6><a style={{color:"black"}} href="mailto:medisureincinerators@gmail.com?Subject=Hello%20Medisure Incinerators">medisureincinerators@gmail.com</a></h6>
-                    {/* <h6>email@sitename.com </h6> */}
-                        <hr  style={{backgroundColor:"#808080"}} />
-                        </div >
-               
+              <input
+                name="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                type="name" className="form-control" id="form-name" placeholder="Name" required />
             </div>
+            <div className="form-group">
+              <i class="fa fa-envelope icon ">  </i> &nbsp;&nbsp;
+                    <label for="form-email">Email</label>
+              <input
+                name="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="email" className="form-control" id="form-email" placeholder="Email Address" required />
+            </div>
+            <div className="form-group">
+              <i class="fas fa-phone"></i>&nbsp;&nbsp;
+                    <label for="form-subject">Contact Number</label>
+              <input
+                name="phone"
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+                type="text" className="form-control" id="form-subject" placeholder="Enter Your Contact No." required />
+            </div>
+            <div className="form-group">
+              <label for="form-message">Email your Message</label>
+              <textarea
+                name="message"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                className="form-control" id="form-message" placeholder="Enter Your Message"></textarea>
+            </div>
+            <button className="btn btn-dark" type="submit">Send <strong><i class="fas fa-greater-than"></i></strong></button>
+
+            <br />
+            <br />
+            <br />
+
+          </form>
+
+        </div>
+
+
+        <div className="col-md-6">
+          <div class="jumbotron"><h1>Our Office</h1></div>
+
+          <div class="panel panel-default">
+            <div class="text-center header">Our Office</div>
+            <div class="panel-body text-center">
+              <h2><i class="far fa-map">  <strong>    Address </strong>   </i></h2>
+              <h6>156/3/2 Bilankheda </h6>
+              <h6>Khandwa</h6>
+              <hr style={{ backgroundColor: "#808080" }} />
+
+              <h2><i class="fas fa-phone-alt">  <strong>    Phone </strong>   </i></h2>
+              <h6><a href='tel:8955162252' style={{ color: "black" }}>+91 9424874278</a></h6>
+              <h6>+91 9999999999 </h6>
+              <hr style={{ backgroundColor: "#808080" }} />
+
+
+              <h2><i class="fa fa-envelope icon">  <strong>   Email</strong>   </i></h2>
+              <h6><a style={{ color: "black" }} href="mailto:medisureincinerators@gmail.com?Subject=Hello%20Medisure Incinerators">medisureincinerators@gmail.com</a></h6>
+              {/* <h6>email@sitename.com </h6> */}
+              <hr style={{ backgroundColor: "#808080" }} />
+            </div >
+
+          </div>
         </div>
         <div className="col-lg-12">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3703.864499162353!2d76.14417691494629!3d21.824165685572897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x829598b51b8cbb02!2sMedisure%20Incinerators!5e0!3m2!1sen!2sin!4v1615662709327!5m2!1sen!2sin" style={{width:"100%", height:"350px",  allowfullscreen:"" ,loading:"lazy"}}></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3703.864499162353!2d76.14417691494629!3d21.824165685572897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x829598b51b8cbb02!2sMedisure%20Incinerators!5e0!3m2!1sen!2sin!4v1615662709327!5m2!1sen!2sin" style={{ width: "100%", height: "350px", allowfullscreen: "", loading: "lazy" }}></iframe>
         </div>
-        
-    </div>
-    <Snackbar
+
+      </div>
+      <Snackbar
         anchorOrigin={{
           vertical: 'bottom',
           horizontal: 'center',
@@ -183,9 +183,9 @@ function Contact() {
           </React.Fragment>
         }
       />
-</div>
+    </div>
 
-    )
+  )
 }
 
 export default Contact
