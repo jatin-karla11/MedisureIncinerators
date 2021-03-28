8381437e599ca683d1451352915029def28dc37b
Jatin Karla <jatinkarla8@gmail.com>
2021-03-28 12:23:09 +0530

    yo

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -6,10 +6,12 @@ function Admin() {
         // user && 
         <div className="admin">
             <h1>admin</h1>
+            
             {/* <div className="admin_header">
                 <div className="admin_header_title">{user.name}</div>
             </div>
-            <div className="admin_body"></div>
+            <div className="admin_body">
+            </div>
             <div className="admin_footer"></div> */}
         </div>
     )
